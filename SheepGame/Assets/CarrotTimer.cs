5da7016644be9db1fcf391e75594e79f097d38c8
Emily Nguyen <emilyhuongnguyen@gmail.com>
2018-04-08 12:18:32 -0700

    Add carrot game

---
+++ SheepGame/Assets/CarrotTimer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrotTimer : MonoBehaviour {
+
+	bool activated;
+	float timeLeft;
+	public bool victory;
+	TextMesh textObject;
+
+	// Use this for initialization
+	void Start () {
+		victory = false;
+		textObject = GameObject.Find ("CarrotTimer").GetComponent<TextMesh> ();
+		textObject.text = "Timer: 30";
+		timeLeft = 30;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (activated) {
+			timeLeft -= Time.deltaTime;
+			textObject.text = "Timer: " + timeLeft;
+			if (timeLeft < 0) {
+				GameOver ();
+			}
+		}
+	}
+
+	void GameOver() {
+		Destroy(GameObject.FindGameObjectWithTag ("Carrot"));
+		if (!victory) {
+			TextMesh carrotText = GameObject.Find ("CarrotEnd").GetComponent<TextMesh> ();
+			carrotText.text = "Failure";
+		}
+	}
+
+	public void activate() {
+		activated = true;
+	}
+}
---
+++ SheepGame/Assets/buttonStuff.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonStuff : MonoBehaviour {
+
+	public int score;
+	string btnName;
+	public CarrotTimer carrotScript;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0 && Input.touches [0].phase == TouchPhase.Began) {
+			Debug.Log ("touch");
+			Ray ray = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+			RaycastHit Hit;
+			if(Physics.Raycast(ray, out Hit)) {
+				btnName = Hit.transform.name;
+				switch(btnName)
+				{
+				case "Carrot":
+					score++;
+					Destroy (Hit.transform.gameObject);
+					Debug.Log ("ok");
+					break;
+				}
+			}
+		}
+
+		if (score >= 25) {
+			Destroy (GameObject.FindGameObjectWithTag ("Carrot"));
+			TextMesh mazeText = GameObject.Find ("CarrotEnd").GetComponent<TextMesh> ();
+			mazeText.text = "You get a point!!";
+			carrotScript.victory = true;
+		}
+	}
+}
