0e87565014b9582ded88529d51551fedb3e013c9
Emily Nguyen <emilyhuongnguyen@gmail.com>
2018-04-08 17:43:19 -0700

    Add crop counting game

---
+++ SheepGame/Assets/CropCount.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CropCount : MonoBehaviour {
+
+
+	public CropTimer2 cropScript;
+	bool victory;
+	bool over;
+	string btnName;
+
+	// Use this for initialization
+	void Start () {
+		victory = false;
+		over = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0 && Input.touches [0].phase == TouchPhase.Began && cropScript.timeLeft <= 0) {
+			Debug.Log ("touch");
+			Ray ray = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+			RaycastHit Hit;
+			if(Physics.Raycast(ray, out Hit)) {
+				btnName = Hit.transform.name;
+				switch(btnName)
+				{
+				case "onionbut":
+					over = true;
+					Debug.Log ("four");
+					Destroy (Hit.transform.gameObject);
+					victory = false;
+					break;
+				case "tomatobut":
+					over = true;
+					Destroy (Hit.transform.gameObject);
+					victory = true;
+					break;
+				case "pizzabut":
+					over = true;
+					Destroy (Hit.transform.gameObject);
+					victory = false;
+					break;
+				}
+			}
+		}
+
+		if (over) {
+			Destroy(GameObject.FindGameObjectWithTag ("button"));
+			TextMesh cropText = GameObject.Find ("CropEnd").GetComponent<TextMesh> ();
+			if (victory) {
+				cropText.text = "Move to next green";
+			} else {
+				cropText.text = "Failure";
+			}
+		}
+	}
+}
---
+++ SheepGame/Assets/CropTimer2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CropTimer2 : MonoBehaviour {
+
+	bool activated;
+	public float timeLeft;
+	TextMesh textObject;
+
+	// Use this for initialization
+	void Start () {
+		textObject = GameObject.Find ("cropTimer").GetComponent<TextMesh> ();
+		textObject.text = "Timer: 5";
+		timeLeft = 5;
+		GameObject.Find ("tomatobut").transform.Translate (Vector3.up * 5000);
+		GameObject.Find("onionbut").transform.Translate(Vector3.up*5000);
+		GameObject.Find ("pizzabut").transform.Translate (Vector3.up*5000);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (activated) {
+			timeLeft -= Time.deltaTime;
+			textObject.text = "Timer: " + timeLeft;
+			if (timeLeft <= 0) {
+				question ();
+			}
+		}
+	}
+
+	void question() {
+		Destroy(GameObject.FindGameObjectWithTag ("Crops"));
+		GameObject.Find ("tomatobut").transform.Translate (Vector3.down * 5000);
+		GameObject.Find("onionbut").transform.Translate(Vector3.down*5000);
+		GameObject.Find ("pizzabut").transform.Translate (Vector3.down*5000);
+		TextMesh mazeText = GameObject.Find ("CropQuestion").GetComponent<TextMesh> ();
+		mazeText.text = "Which object appears the most?";
+	}
+
+	public void activate() {
+		activated = true;
+	}
+}
